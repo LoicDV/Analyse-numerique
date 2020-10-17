c32cf112684f52cd0f03dc41d89c0bb68a2871a7
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-10-17 16:46:08 +0200

    PUTAIN DE 0.

--- TP1/rootFinding.ml
+++ TP1/rootFinding.ml
@@ -1,37 +1,37 @@
-(* import Root1d *)
+open Root1d;;
 
 let rootFinding f df a b =
     (* Rappel : nous ne pouvons avoir que MAX 2 racines car unimodale *)
     let fa = f a and fb = f b and dfa = df f a and dfb = df f b in
 
-        if (fa < 0 && fb > 0) || (fa < 0 && fb = 0 && dfb > 0) then
+        if (fa < 0. && fb > 0.) || (fa < 0. && fb = 0. && dfb > 0.) then
             let x = Root1d.brent f a b in
                 x;
 
-        if (fa > 0 && fb < 0) || (fa = 0 && fb < 0 && dfa < 0) then
+        if (fa > 0. && fb < 0.) || (fa = 0. && fb < 0. && dfa < 0.) then
             let x = Root1d.brent f a b in
                 x;
 
-        if fa = 0 && fb = 0 then
+        if fa = 0. && fb = 0. then
             a; b;
 
-        if fa = 0 && fb > 0 then
+        if fa = 0. && fb > 0. then
             a;
 
-        if fa > 0 && fb = 0 then
+        if fa > 0. && fb = 0. then
             b;
 
-        if fa < 0 && fb = 0 && dfb < 0 then
+        if fa < 0. && fb = 0. && dfb < 0. then
             let m = Root1d.brent df a b in
                 let x = Root1d.brent f a m in
                     x; m;
 
-        if fa = 0 && fb < 0 && dfa > 0 then
+        if fa = 0. && fb < 0. && dfa > 0. then
             let m = Root1d.brent df a b in
                 let x = Root1d.brent f m b in
                     a; x;
 
-        if fa < 0 && fb < 0 then
+        if fa < 0. && fb < 0. then
             let m = Root1d.brent df a b in
                 let x = Root1d.brent f a m and y = Root1d.brent f m b in
                     x; y;
