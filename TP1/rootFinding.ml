d7400c331a73fd313b2005eb63158e93414a64e7
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-10-17 17:40:15 +0200

    Voila

--- TP1/rootFinding.ml
+++ TP1/rootFinding.ml
@@ -1,15 +1,15 @@
-open Root1d;;
+open Root1D
 
 let rootFinding f df a b =
     (* Rappel : nous ne pouvons avoir que MAX 2 racines car unimodale *)
     let fa = f a and fb = f b and dfa = df a and dfb = df b in
 
         if (fa < 0. && fb > 0.) || (fa < 0. && fb = 0. && dfb > 0.) then
-            let x = Root1d.brent f a b in
+            let x = Root1D.brent f a b in
                 x;
 
         if (fa > 0. && fb < 0.) || (fa = 0. && fb < 0. && dfa < 0.) then
-            let x = Root1d.brent f a b in
+            let x = Root1D.brent f a b in
                 x;
 
         if fa = 0. && fb = 0. then
@@ -22,18 +22,18 @@ let rootFinding f df a b =
             b;
 
         if fa < 0. && fb = 0. && dfb < 0. then
-            let m = Root1d.brent df a b in
-                let x = Root1d.brent f a m in
+            let m = Root1D.brent df a b in
+                let x = Root1D.brent f a m in
                     x; m;
 
         if fa = 0. && fb < 0. && dfa > 0. then
-            let m = Root1d.brent df a b in
-                let x = Root1d.brent f m b in
+            let m = Root1D.brent df a b in
+                let x = Root1D.brent f m b in
                     a; x;
 
         if fa < 0. && fb < 0. && (dfa *. dfb) < 0. then
-            let m = Root1d.brent df a b in
-                let x = Root1d.brent f a m and y = Root1d.brent f m b in
+            let m = Root1D.brent df a b in
+                let x = Root1D.brent f a m and y = Root1D.brent f m b in
                     x; y;
 
         None;;
