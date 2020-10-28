366a84108d89f07c7d3c13dc62ad201272d7528d
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-10-28 15:39:02 +0100

    Ajustement des lignes MAX 80 char

--- TP1/q5.ml
+++ TP1/q5.ml
@@ -8,37 +8,35 @@ let q5 a b c =
 
     if a = 0. then
         match (b, c) with
-            (0., c) -> print_string "no roots"                              (*(0,0,c)   Cas 1*) 
-           |(b, 0.) -> print_float 0.;                                      (*(0,b,0)   Cas 2*)
-           |(b, c)  -> let x = -.c /. b in print_float x;                   (*(0,b,c)        *)
+            (0., c) -> print_string "no roots"            (*(0,0,c)   Cas 1*)
+           |(b, 0.) -> print_float 0.;                    (*(0,b,0)   Cas 2*)
+           |(b, c)  -> let x = -.c /. b in print_float x; (*(0,b,c)        *)
 
     if b = 0. then
         match (a, c) with
-            (0., c) -> print_string "no roots"                              (*(0,0,c)   Cas 1*)
-           |(a, 0.) -> print_float 0.;                                      (*(a,0,0)   Cas 3*)
-           |(a, c)  -> if (a < 0. && c > 0.) || (a > 0. && c < 0.) then     (*(a,0,c)        *)
-                           let x1 = sqrt (-.c /. a) and
-                           x2 = -.sqrt (-.c /. a) in
-                               print_float x1;
-                               print_string"\n";
-                               print_float x2;
+            (0., c) -> print_string "no roots"            (*(0,0,c)   Cas 1*)
+           |(a, 0.) -> print_float 0.;                    (*(a,0,0)   Cas 3*)
+           |(a, c)  ->                                    (*(a,0,c)        *)
+            if (a < 0. && c > 0.) || (a > 0. && c < 0.) then
+                let x1 = sqrt (-.c /. a) and x2 = -.sqrt (-.c /. a) in
+                    print_float x1;
+                    print_string"\n";
+                    print_float x2;
 
     if c = 0. then
         match (a, b) with
-            (0., b) -> print_float 0.;                                      (*(0,b,0)   Cas 2*)
-           |(a, 0.) -> print_float 0.;                                      (*(a,0,0)   Cas 3*)
-           |(a, b) -> let x = -.b /. a in                                   (*(a,b,0)        *)
+            (0., b) -> print_float 0.;                    (*(0,b,0)   Cas 2*)
+           |(a, 0.) -> print_float 0.;                    (*(a,0,0)   Cas 3*)
+           |(a, b) -> let x = -.b /. a in                 (*(a,b,0)        *)
                           print_float 0.;
                           print_float x;
 (*Il y a des cas qui se répetent dans les 3 if ci dessus*)
 
 
-
-
     if a != 0. && b != 0. && c != 0. then
     (* -b/a = somme des racines , c/a = produit des racines ,
-        si c/a>0 alors -b/a peut etre utiliser comme borne car somme de deux positifs >= max{des deux positifs} (de meme pour negatifs)
-        *)
+        si c/a>0 alors -b/a peut etre utiliser comme borne car somme
+        de deux positifs >= max{des deux positifs} (de meme pour negatifs) *)
         if a > 0. then
             let aa = -.a and bb = -.b and cc = -.c in
                 let f x = aa *. x ** 2. +. bb *. x +. cc and
