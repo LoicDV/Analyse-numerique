ea240f503e30c25cfe0fcd38e214f17f4fc2ce48
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-10-31 18:19:03 +0100

    Mmmmmmmh

--- TP1/q5.ml
+++ TP1/q5.ml
@@ -7,27 +7,31 @@ let display liste =
 let change_sign z =
     (-.z);;
 
+let q5_ a b c =
+    let f x = a *. x ** 2. +. b *. x +. c and
+            df x = 2. *. x *. a +. b in
+            let borne_d = max((4. *. abs_float(b)) /. abs_float(a),
+                            (2. *. sqrt(abs_float(c)) /. abs_float(a))) in
+                let borne_g = (-.borne_d) in
+                (* 2. *. -.b/.(2.*.a) -. borne_d *)
+                    let liste = RootFinding.rootFinding f df borne_g borne_d in
+                    liste;;
+
 let q5 a b c =
     if a = 0. then
         match (b, c) with
             (0., c) -> [];
            |(b, 0.) -> [0.];
            |(b, c)  -> let x = -.c /. b in [x];
-
     else
         if a > 0. then
-            let a = change_sign a
-            let b = change_sign b
-            let c = change_sign c;;
-
-        let f x = a *. x ** 2. +. b *. x +. c and
-            df x = 2. *. x *. a +. b in
-            let borne_d = max (4. *. abs_float(b) /. abs_float(a)),
-                            2. *. sqrt(abs_float(c) /. abs_float(a)) in
-                let borne_g = (-.borne_d) in
-                (* 2. *. -.b/.(2.*.a) -. borne_d *)
-                    liste = RootFinding.rootFinding f df borne_g borne_d
-                    liste;;
+            let a = change_sign a;
+            let b = change_sign b;
+            let c = change_sign c;
+            let liste = q5_ a b c;
+            else
+                let liste = q5_ a b c in
+    liste;;
 
 let () =
     let a = Sys.argv 2;
