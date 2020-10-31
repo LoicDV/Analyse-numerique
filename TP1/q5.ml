a7e442e8567f469bda82552e5062cc85dd26703d
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-10-31 18:06:09 +0100

    Ouille

--- TP1/q5.ml
+++ TP1/q5.ml
@@ -4,9 +4,8 @@ open Printf
 let display liste =
     List.iter (printf "%.8f\n") liste;;
 
-let change_sign a b c =
-    let a = -.a and b = -.b and c = -.c in
-    a, b, c;;
+let change_sign z =
+    (-.z);;
 
 let q5 a b c =
     if a = 0. then
@@ -15,14 +14,18 @@ let q5 a b c =
            |(b, 0.) -> [0.];
            |(b, c)  -> let x = -.c /. b in [x];
 
-    else if a > 0. then
-        let a, b, c = change_sign a b c;
+    else
+        if a > 0. then
+            let a = change_sign a
+            let b = change_sign b
+            let c = change_sign c;;
 
         let f x = a *. x ** 2. +. b *. x +. c and
             df x = 2. *. x *. a +. b in
-            let borne_d = max (4. *. abs(b) /. abs(a)),
-                                    2. *. sqrt(abs(c) /. abs(a)) in
-                let borne_g = 2. *. -.b/.(2.*.a) -. borne_d in
+            let borne_d = max (4. *. abs_float(b) /. abs_float(a)),
+                            2. *. sqrt(abs_float(c) /. abs_float(a)) in
+                let borne_g = (-.borne_d) in
+                (* 2. *. -.b/.(2.*.a) -. borne_d *)
                     liste = RootFinding.rootFinding f df borne_g borne_d
                     liste;;
 
