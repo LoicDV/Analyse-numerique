8e5b2639a0f18f668243a26aefb6c910687e3410
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-11-01 12:29:59 +0100

    Problème rootFinding module

--- TP1/q5.ml
+++ TP1/q5.ml
@@ -3,9 +3,6 @@ open Printf
 let display liste =
     List.iter (printf "%.8f\n") liste;;
 
-let change_sign z =
-    (-.z);;
-
 let q5_ a b c =
     let f x = a *. x ** 2. +. b *. x +. c and
             df x = 2. *. x *. a +. b in
@@ -29,6 +26,8 @@ let q5 a b c =
 
 let () =
     let array = Sys.argv in
-        let a = array[1] and b = array[2] and c = array[3] in
+        let a = float_of_string array.(2) and
+        b = float_of_string array.(3) and
+        c = float_of_string array.(4) in
             let liste = q5 a b c in
                 display liste;;
