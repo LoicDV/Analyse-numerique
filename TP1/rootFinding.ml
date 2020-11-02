c112cab6790485a7e87603a63538fbf767830641
Rayghun <200103@umons.ac.be>
2020-11-02 18:50:43 +0100

    RootFinding : Cas dérivée nulle aux extrémités

--- TP1/rootFinding.ml
+++ TP1/rootFinding.ml
@@ -1,41 +1,40 @@
+let rec rootDeriv df a b =
+    let dfa = df a and dfb = df b in
+        if dfa > 0. && dfb < 0. then
+            Root1D.brent df a b
+        else
+            let mid = ((a +. b) /. 2.) in
+                let dfm = df mid in
+                    if dfm = 0. then
+                        mid
+                    else if dfm > 0. then
+                        rootDeriv df mid b
+                    else
+                        rootDeriv df a mid;;
+
 let rootFinding f df a b =
-    (* Rappel : nous ne pouvons avoir que MAX 2 racines car unimodale *)
-    let fa = f a and fb = f b and dfa = df a and dfb = df b in
-
-        if (fa < 0. && fb > 0.) || (fa < 0. && fb = 0. && dfb > 0.) then
-            let x = Root1D.brent f a b in
-                [x]
-
-        else if (fa > 0. && fb < 0.) || (fa = 0. && fb < 0. && dfa < 0.) then
-            let x = Root1D.brent f a b in
-                [x]
-
-        else if fa = 0. && fb = 0. then
-            [a; b]
-
+    let fa = f a and fb = f b in
+        if fa > 0. && fb > 0. then
+            []
         else if fa = 0. && fb > 0. then
             [a]
-
         else if fa > 0. && fb = 0. then
             [b]
-
-        else if fa < 0. && fb = 0. && dfb < 0. then
-            let m = Root1D.brent df a b in
-                let x = Root1D.brent f a m in
-                    [x; b]
-
-        else if fa = 0. && fb < 0. && dfa > 0. then
-            let m = Root1D.brent df a b in
-                let x = Root1D.brent f m b in
-                    [a; x]
-
-        else if fa < 0. && fb < 0. && (dfa *. dfb) < 0. then
-            let m = Root1D.brent df a b in
-                if f m >= 0. then
-                    let x = Root1D.brent f a m and y = Root1D.brent f m b in
-                        [x; y]
-                else
-                    [];
-
         else
-	    [];;
+            let m = rootDeriv df a b in
+                if fa > 0. && fb <= 0. then
+                    let x = Root1D.brent f m b in
+                        [x]
+                else if fa <= 0. && fb > 0. then
+                    let x = Root1D.brent f a m in
+                        [x]
+                else
+                    let fm = f m in
+                        if fm > 0. then
+                            let x = Root1D.brent f a m and
+                                y = Root1D.brent f m b in
+                                [x; y]
+                        else if fm = 0. then
+                            [m]
+                        else
+                            [];;
