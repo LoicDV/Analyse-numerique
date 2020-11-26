8297d6d29987a1dc50aa9331837b34564cb2c587
Rayghun <200103@umons.ac.be>
2020-11-26 10:01:16 +0100

    q1.ml : Code fonctionnel

--- TP2/src/q1.ml
+++ TP2/src/q1.ml
@@ -1,15 +1,25 @@
-let q1 a =
-  a;;
+open Lacaml.D
+open Lacaml.Io
+open Format
 
-let display array =
-  Array.iter (Array.iter (Printf.printf "%.8f\n")) array;;
+let q1 a b =
+  let a = Mat.of_array a in
+  let b = Mat.of_array b in
+  let ata = gemm ~transa:`T a a in
+  let atb = gemm ~transa:`T a b in
+  gesv ata atb;
+  printf "@[%a@]@\n" pp_fmat atb;;
+
 
 let () = 
   let m = int_of_string Sys.argv.(1) and n = int_of_string Sys.argv.(2) in
-  let a = Array.make_matrix m n 0. in
+  let a = Array.make_matrix m n 0. and b = Array.make_matrix m 1 0. in
   for i = 0 to (m - 1) do
     for j = 0 to (n - 1) do
-      a.(i).(j) <- float_of_string Sys.argv.(3 + (i * n) + j)
+      a.(i).(j) <- Float.of_string Sys.argv.(3 + (i * n) + j)
     done;
   done;
-  display (q1 a);;
+  for i = 0 to (m - 1) do
+    b.(i).(0) <- Float.of_string Sys.argv.(3 + (m * n) + i)
+  done;
+  q1 a b;;
