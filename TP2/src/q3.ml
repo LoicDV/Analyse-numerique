9f82e26197da3b5663c125eeb3f9df46d057392e
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-11-26 15:36:05 +0100

    dérivée des matrices A et B

--- TP2/src/q3.ml
+++ TP2/src/q3.ml
@@ -14,10 +14,24 @@ let assign_mat mat_a mat_b t =
   mat_b.{3, 1} <- 1. /. (1. +. t ** 2.);
   ;;
 
+let assign_mat_deriv d_mat_a d_mat_b t =
+  d_mat_a.{1, 1} <- cos t;
+  d_mat_a.{1, 2} <- 2.;
+  d_mat_a.{2, 1} <- 1.;
+  d_mat_a.{2, 2} <- exp t;
+  d_mat_a.{3, 1} <- 1. /. (1. +. t ** 2.);
+  d_mat_a.{3, 2} <- 1.;
+  d_mat_b.{1, 1} <- 1.;
+  d_mat_b.{2, 1} <- 2. *. t;
+  d_mat_b.{3, 1} <- -2. *. t /. (1. +. t ** 2.) ** 2.;
+  ;;
+
 let q3 t =
-  let mat_a = Mat.create 3 2 and mat_b = Mat.create 3 1 in
+  let mat_a = Mat.create 3 2 and mat_b = Mat.create 3 1 and
+      d_mat_a = Mat.create 3 2 and d_mat_b = Mat.create 3 1 in
     assign_mat mat_a mat_b t;
-    q2 mat_a mat_b;;
+    assign_mat_deriv d_mat_a d_mat_b t;
+    q2.q2 mat_a mat_b d_mat_a d_mat_b t;;
 
 let () =
   let t = Float.of_string(Sys.argv.(1)) in
