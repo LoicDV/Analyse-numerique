4fabd8370a8f728757398e9931ccd60467eecaee
Rayghun <200103@umons.ac.be>
2020-11-26 11:27:34 +0100

    q1.ml : Code utilisable pour les autres questions

--- TP2/src/q1.ml
+++ TP2/src/q1.ml
@@ -3,23 +3,19 @@ open Lacaml.Io
 open Format
 
 let q1 a b =
-  let a = Mat.of_array a in
-  let b = Mat.of_array b in
-  let ata = gemm ~transa:`T a a in
-  let atb = gemm ~transa:`T a b in
-  gesv ata atb;
-  printf "@[%a@]@\n" pp_fmat atb;;
+  let sol = gemm ~transa:`T a b in
+  gesv (gemm ~transa:`T a a) sol;
+  sol;;
 
-
-let () = 
+let () =
   let m = int_of_string Sys.argv.(1) and n = int_of_string Sys.argv.(2) in
-  let a = Array.make_matrix m n 0. and b = Array.make_matrix m 1 0. in
+  let a = Mat.create m n and b = Mat.create m 1 in
   for i = 0 to (m - 1) do
     for j = 0 to (n - 1) do
-      a.(i).(j) <- Float.of_string Sys.argv.(3 + (i * n) + j)
+      a.{i + 1, j + 1} <- (Float.of_string Sys.argv.(3 + (i * n) + j))
     done;
   done;
   for i = 0 to (m - 1) do
-    b.(i).(0) <- Float.of_string Sys.argv.(3 + (m * n) + i)
+    b.{i + 1, 1} <- (Float.of_string Sys.argv.(3 + (m * n) + i))
   done;
-  q1 a b;;
+  printf "@[%a@]@\n" pp_fmat (q1 a b);;
