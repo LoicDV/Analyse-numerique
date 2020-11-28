8daa9bb00740eb41c85aa9c1817a23da14bb7a6a
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-11-28 18:02:29 +0100

    Correction des commenataire + suppression du .mli

--- TP2/src/q2.mli
+++
@@ -1,8 +0,0 @@
-val deriv : ('a -> 'a array array) -> ('a -> 'a array array) ->
-    ('a -> 'a array array) -> ('a -> 'a array array) -> float -> 'a array array
-(** Returns an approximation of the derivative of the function x(t)
-    in the form of a matrix.
-
-    The function thus takes 4 parameters which are functions that
-    send a real t in a matrix and also as another parameter, the t in question.
-*)
