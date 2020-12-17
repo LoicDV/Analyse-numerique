2e001806a10db99dcd93d3fe914bcd7f1ee06e65
LoicDV <Loic.DUPONT@student.umons.ac.be>
2020-12-17 22:57:39 +0100

    Commentaire (à vérifier)

--- TP3/src/pendulum.mli
+++ TP3/src/pendulum.mli
@@ -5,9 +5,13 @@ val to_vec : float array -> vec
             (* Returns a object vector by taking the element of float array.
             *)
 
-val solve : float -> float -> float -> float -> float ->
+val solve : ?full:bool -> float -> float -> float -> float -> float ->
             (float -> float -> float -> float -> float -> float) ->
             float -> float -> float -> float array
-            (* ...
+            (*Return a fonction who takes 3 arguments, x_0, Θ_0 and
+              t and this function returns a vector (x(t), Θ(t)) who is
+              solution of the EDO :
 
+              A ∂^2_t (x) = f(t, x, ∂_t x, Θ, ∂_t Θ)
+                      (Θ)
             *)
